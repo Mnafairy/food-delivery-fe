c635ea748c3f5e59fa0f57a536c516a39db763c5
Orgil <mnafairy@gmail.com>
2024-03-20 20:51:51 +0800

    fixing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,104 @@
-import { Box, Stack } from "@mui/material";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  InputBase,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AdbIcon from "@mui/icons-material/Adb";
 
-export const Header = () => (
-  <Box>
-    <Stack>header</Stack>
-  </Box>
-);
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 8,
+  backgroundColor: alpha(theme.palette.common.white, 0.5),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            НҮҮР
+          </Typography>
+          <Search sx={{ border: 1, borderColor: "black" }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Хайх"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+// export const Header = () => (
+//   <Box>
+//     <Stack>header</Stack>
+//   </Box>
+// );
