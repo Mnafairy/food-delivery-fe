e2911bacded9d1547f9cab9a365eb9db4616f186
Orgil <mnafairy@gmail.com>
2024-03-24 22:19:37 +0800

    home

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,7 +3,13 @@ import { Stack } from "@mui/material";
 
 const Page = () => {
   return (
-    <Stack justifyContent="center" alignItems="center" py={4}>
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      py={4}
+      mt="55px"
+      mb="100px"
+    >
       <Signup />
     </Stack>
   );
